b090710eb6548f290ac45783283121ee57e86324
Dávid Bejó <bejodavid123@gmail.com>
2021-11-28 16:29:49 +0100

    labels changing according to selected item in combobox

--- Beadando3/UserControlls/UC_Feladat2.cs
+++ Beadando3/UserControlls/UC_Feladat2.cs
@@ -16,5 +16,48 @@ namespace Beadando3.UserControlls
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                label1.Text = "EUR";
+                label2.Text = "USD";
+                label3.Text = "GPB";
+                label4.Text = "CYN";
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                label1.Text = "HUF";
+                label2.Text = "USD";
+                label3.Text = "GPB";
+                label4.Text = "CYN";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                label1.Text = "EUR";
+                label2.Text = "HUF";
+                label3.Text = "GPB";
+                label4.Text = "CYN";
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                label1.Text = "EUR";
+                label2.Text = "USD";
+                label3.Text = "HUF";
+                label4.Text = "CYN";
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                label1.Text = "EUR";
+                label2.Text = "USD";
+                label3.Text = "GPB";
+                label4.Text = "HUF";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
