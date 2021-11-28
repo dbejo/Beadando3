11cec78ad675fc1f87022e47ac756b8194d4986b
Dávid Bejó <bejodavid123@gmail.com>
2021-11-28 17:23:58 +0100

    Message when input contains .

--- Beadando3/UserControlls/UC_Feladat2.cs
+++ Beadando3/UserControlls/UC_Feladat2.cs
@@ -62,20 +62,83 @@ namespace Beadando3.UserControlls
                                                    { 302.36, 0.85, 1.0, 0.76, 6.63 },
                                                    { 399.52, 1.12, 1.32, 1.0, 8.76 },
                                                    { 45.60, 0.13, 0.15, 0.11, 1.0 } };
-            double number = double.Parse(textBox1.Text);
-            if (comboBox1.SelectedIndex == 0)
+            if (textBox1.Text.Contains("."))
             {
-                double num1 = number * szorzomatrix[0, 1];
-                double num2 = number * szorzomatrix[0, 2];
-                double num3 = number * szorzomatrix[0, 3];
-                double num4 = number * szorzomatrix[0, 4];
-
-                textBox2.Text = num1.ToString();
-                textBox3.Text = num2.ToString();
-                textBox4.Text = num3.ToString();
-                textBox5.Text = num4.ToString();
+                MessageBox.Show("Please use comma (,) instead of dot (.)", "Error", MessageBoxButtons.OK);
+                textBox1.Text = textBox1.Text.Replace('.', ',');
             }
+            else
+            { 
+                if (comboBox1.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please choose a currency!", "Error", MessageBoxButtons.OK);
+                }
+                else if (comboBox1.SelectedIndex == 0)
+                {
+                    double number = double.Parse(textBox1.Text);
+                    double num1 = number * szorzomatrix[0, 1];
+                    double num2 = number * szorzomatrix[0, 2];
+                    double num3 = number * szorzomatrix[0, 3];
+                    double num4 = number * szorzomatrix[0, 4];
 
+                    textBox2.Text = num1.ToString();
+                    textBox3.Text = num2.ToString();
+                    textBox4.Text = num3.ToString();
+                    textBox5.Text = num4.ToString();
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    double number = double.Parse(textBox1.Text);
+                    double num1 = number * szorzomatrix[1, 0];
+                    double num2 = number * szorzomatrix[1, 2];
+                    double num3 = number * szorzomatrix[1, 3];
+                    double num4 = number * szorzomatrix[1, 4];
+
+                    textBox2.Text = num1.ToString();
+                    textBox3.Text = num2.ToString();
+                    textBox4.Text = num3.ToString();
+                    textBox5.Text = num4.ToString();
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    double number = double.Parse(textBox1.Text);
+                    double num1 = number * szorzomatrix[2, 1];
+                    double num2 = number * szorzomatrix[2, 0];
+                    double num3 = number * szorzomatrix[2, 3];
+                    double num4 = number * szorzomatrix[2, 4];
+
+                    textBox2.Text = num1.ToString();
+                    textBox3.Text = num2.ToString();
+                    textBox4.Text = num3.ToString();
+                    textBox5.Text = num4.ToString();
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    double number = double.Parse(textBox1.Text);
+                    double num1 = number * szorzomatrix[3, 1];
+                    double num2 = number * szorzomatrix[3, 2];
+                    double num3 = number * szorzomatrix[3, 0];
+                    double num4 = number * szorzomatrix[3, 4];
+
+                    textBox2.Text = num1.ToString();
+                    textBox3.Text = num2.ToString();
+                    textBox4.Text = num3.ToString();
+                    textBox5.Text = num4.ToString();
+                }
+                else if (comboBox1.SelectedIndex == 4)
+                {
+                    double number = double.Parse(textBox1.Text);
+                    double num1 = number * szorzomatrix[4, 1];
+                    double num2 = number * szorzomatrix[4, 2];
+                    double num3 = number * szorzomatrix[4, 3];
+                    double num4 = number * szorzomatrix[4, 0];
+
+                    textBox2.Text = num1.ToString();
+                    textBox3.Text = num2.ToString();
+                    textBox4.Text = num3.ToString();
+                    textBox5.Text = num4.ToString();
+                }
+            }
         }
     }
 }
