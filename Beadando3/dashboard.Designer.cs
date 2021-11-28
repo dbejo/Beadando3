4f529c2cca9c8f24ca5e8186e5f1761f6f032742
Dávid Bejó <bejodavid123@gmail.com>
2021-11-28 17:49:03 +0100

    2021.11.28

--- Beadando3/dashboard.Designer.cs
+++ Beadando3/dashboard.Designer.cs
@@ -29,25 +29,18 @@ namespace Beadando3
         /// </summary>
         private void InitializeComponent()
         {
-            this.uC_Feladat11 = new Beadando3.UserControlls.UC_Feladat1();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.uC_Feladat21 = new Beadando3.UserControlls.UC_Feladat2();
+            this.uC_Feladat11 = new Beadando3.UserControlls.UC_Feladat1();
             this.SuspendLayout();
-            // 
-            // uC_Feladat11
-            // 
-            this.uC_Feladat11.BackColor = System.Drawing.Color.White;
-            this.uC_Feladat11.Location = new System.Drawing.Point(182, 1);
-            this.uC_Feladat11.Name = "uC_Feladat11";
-            this.uC_Feladat11.Size = new System.Drawing.Size(800, 800);
-            this.uC_Feladat11.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(164, 57);
             this.button1.TabIndex = 1;
             this.button1.Text = "1. Feladat";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,9 +48,10 @@ namespace Beadando3
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(12, 79);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(163, 57);
             this.button2.TabIndex = 2;
             this.button2.Text = "2. Feladat";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,6 +64,16 @@ namespace Beadando3
             this.uC_Feladat21.Name = "uC_Feladat21";
             this.uC_Feladat21.Size = new System.Drawing.Size(800, 800);
             this.uC_Feladat21.TabIndex = 3;
+            this.uC_Feladat21.Visible = false;
+            // 
+            // uC_Feladat11
+            // 
+            this.uC_Feladat11.BackColor = System.Drawing.Color.White;
+            this.uC_Feladat11.Location = new System.Drawing.Point(182, 1);
+            this.uC_Feladat11.Name = "uC_Feladat11";
+            this.uC_Feladat11.Size = new System.Drawing.Size(800, 800);
+            this.uC_Feladat11.TabIndex = 0;
+            this.uC_Feladat11.Visible = false;
             // 
             // dashboard
             // 
