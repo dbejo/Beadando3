d097d7a717396a7498011795f3c51ea4124ac5e4
Dávid Bejó <bejodavid123@gmail.com>
2021-11-28 16:49:57 +0100

    2021.11.28

--- Beadando3/UserControlls/UC_Feladat2.cs
+++ Beadando3/UserControlls/UC_Feladat2.cs
@@ -57,6 +57,24 @@ namespace Beadando3.UserControlls
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            double[,] szorzomatrix = new double[5, 5] { { 1.0, 0.0028, 0.0033, 0.0025, 0.022 },
+                                                   { 355.36, 1.0, 1.18, 0.89, 7.79 },
+                                                   { 302.36, 0.85, 1.0, 0.76, 6.63 },
+                                                   { 399.52, 1.12, 1.32, 1.0, 8.76 },
+                                                   { 45.60, 0.13, 0.15, 0.11, 1.0 } };
+            double number = double.Parse(textBox1.Text);
+            if (comboBox1.SelectedIndex == 0)
+            {
+                double num1 = number * szorzomatrix[0, 1];
+                double num2 = number * szorzomatrix[0, 2];
+                double num3 = number * szorzomatrix[0, 3];
+                double num4 = number * szorzomatrix[0, 4];
+
+                textBox2.Text = num1.ToString();
+                textBox3.Text = num2.ToString();
+                textBox4.Text = num3.ToString();
+                textBox5.Text = num4.ToString();
+            }
 
         }
     }
